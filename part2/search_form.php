7ec7eccee02d5378b138ad199f7c7c0a67512c13
Thomas Nguyen <tn2thomasnguyen@gmail.com>
2026-05-20 22:21:39 +1000

    added search in search reuslts

---
+++ part2/search_form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Form</title>
+</head>
+<body>
+
+<form action="search_results.php" method="GET">
+    <label for="search">Search:</label>
+
+    <input type="text" id="search" name="search">
+
+    <input type="submit" value="Search">
+</form>
+
+</body>
+</html>
